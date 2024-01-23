5fdb50a2647ab4eb9ebadfebd457a51a3708ab5d
alejosatizabal <alejosatizabal@yahoo.es>
2024-01-23 17:48:42 -0500

    parte1.c / Manejo de eventos

--- part1/part1..5/src/App.jsx
+++ part1/part1..5/src/App.jsx
@@ -1,16 +1,35 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [ counter, setCounter ] = useState(0);
 
+  const handleClick = () => {
+    console.log('clicked');
+    setCounter(counter + 1);
+  };
+  
+  /*
   setTimeout(
     () => setCounter(counter + 1),
     1000
-  )
+  );
+  */
+
   console.log('rendering...', counter)
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={handleClick}>
+        plus con función
+      </button>
+      <button onClick={ () => setCounter(counter + 1) }>
+        plus, directamente
+      </button>
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
+    </div>
   )
-}
+};
 
-export default App
+export default App;
