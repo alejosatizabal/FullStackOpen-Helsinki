ef9f83b7f78d3ad814275487ec2d9e671884b555
alejosatizabal <alejosatizabal@yahoo.es>
2024-01-23 18:40:34 -0500

    parte1.c / Pasando el estado a componentes hijos

--- part1/part1..5/src/App.jsx
+++ part1/part1..5/src/App.jsx
@@ -1,28 +1,34 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  // <button onClick={props.handleClick}>
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [ counter, setCounter ] = useState(0);
 
   const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0);
   
-  /*
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  );
-  */
-
   console.log('rendering...', counter)
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text='plus'/>
+      <Button onClick={setToZero} text='zero'/>
+      <Button onClick={decreaseByOne} text='minus'/>
     </div>
   )
 };
