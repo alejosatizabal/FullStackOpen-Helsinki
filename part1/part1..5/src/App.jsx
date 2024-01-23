75e571257bd02d0d8aa4153d3ade909015cdf166
alejosatizabal <alejosatizabal@yahoo.es>
2024-01-23 17:38:54 -0500

    parte1.c / Componente con estado

--- part1/part1..5/src/App.jsx
+++ part1/part1..5/src/App.jsx
@@ -1,67 +1,15 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>Name Course: {props.course.name}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <h1>Parts:</h1>
-        <Part part={props.course.parts[0].name} numxpart={props.course.parts[0].exercises} />
-        <Part part={props.course.parts[1].name} numxpart={props.course.parts[1].exercises} />
-        <Part part={props.course.parts[2].name} numxpart={props.course.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.part}, exercises {props.numxpart}</p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-console.log(props)
-    const totalExercises =  props.course.parts[0].exercises +
-                            props.course.parts[1].exercises +
-                            props.course.parts[2].exercises
-  return (
-    <div>
-      <h1>Number of exercises: {totalExercises}</h1>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [ counter, setCounter ] = useState(0)
 
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+  console.log('rendering...', counter)
   return (
-    <div>
-        <Header course={course}/>
-        <Content course={course}/>
-        <Total course={course}/>
-    </div>
+    <div>{counter}</div>
   )
 }
 
