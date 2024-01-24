a9e45998612f4eb54def54f51abf07ba1022198c
alejosatizabal <alejosatizabal@yahoo.es>
2024-01-24 17:23:03 -0500

    parte1.d / La actualización del estado es asíncrona

--- part1/part1..5/src/App.jsx
+++ part1/part1..5/src/App.jsx
@@ -19,22 +19,33 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
+  // La actualización del estado no es sincronica, así se puede corregir
   const handleLeftClick = () => {
-    setAll(allClicks.concat('L')),
-    setLeft(left + 1)  }
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
 
+  // Se evidencia que la actualización del estado no es sincronica
   const handleRightClick = () => {
     setAll(allClicks.concat('R')),
-    setRight(right + 1)  }
-    
+    console.log('right before', right),
+    setRight(right + 1)
+    console.log('right after', right),
+    setTotal(left + right)
+  }
+
   return (
     <div>
       {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(' ')}</p>    </div>
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>    </div>
   )
 }
 ;
