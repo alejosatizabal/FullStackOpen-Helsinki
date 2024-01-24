280f82a9cc65fc61e44faa5c87490bb3c3b2d8eb
alejosatizabal <alejosatizabal@yahoo.es>
2024-01-24 17:00:29 -0500

    parte1.d / Estado complejo

--- part1/part1..5/src/App.jsx
+++ part1/part1..5/src/App.jsx
@@ -16,21 +16,38 @@ const Button = (props) => {
 }
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0,
+    //otro:0
+  })
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0);
+  const handleLeftClick = () => {
+    const newClicks = {
+      ...clicks,
+      left: clicks.left + 1
+    }
+    setClicks(newClicks)
+  }
   
-  console.log('rendering...', counter)
+  const handleRightClick = () => {
+    const newClicks = {
+      ...clicks,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
+
+  //console.log(clicks.otro)
+
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text='plus'/>
-      <Button onClick={setToZero} text='zero'/>
-      <Button onClick={decreaseByOne} text='minus'/>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
-};
+}
+;
 
 export default App;
