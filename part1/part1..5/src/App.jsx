117acc99edd0e22b18c320022c659a880a5b448e
alejosatizabal <alejosatizabal@yahoo.es>
2024-01-24 17:10:17 -0500

    parte1.d / Manejo de matrices

--- part1/part1..5/src/App.jsx
+++ part1/part1..5/src/App.jsx
@@ -16,36 +16,25 @@ const Button = (props) => {
 }
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0,
-    //otro:0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
   const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks,
-      left: clicks.left + 1
-    }
-    setClicks(newClicks)
-  }
-  
+    setAll(allClicks.concat('L')),
+    setLeft(left + 1)  }
+
   const handleRightClick = () => {
-    const newClicks = {
-      ...clicks,
-      right: clicks.right + 1
-    }
-    setClicks(newClicks)
-  }
-
-  //console.log(clicks.otro)
-
+    setAll(allClicks.concat('R')),
+    setRight(right + 1)  }
+    
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
-    </div>
+      {right}
+      <p>{allClicks.join(' ')}</p>    </div>
   )
 }
 ;
