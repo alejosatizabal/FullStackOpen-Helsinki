71755b64fd066ecd93b0f811b9259f9a6113d247
alejosatizabal <alejosatizabal@yahoo.es>
2024-01-25 20:04:02 -0500

    1.6: unicafe, paso 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,20 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+const Titulos = (props) => {
+  return(
+    <h1>{props.titulo}</h1>
+  )
+}
+
+const Estadisticas = (props) => {
+  return(
+    <div>
+      <div>Good: { props.resenas.good }</div>
+      <div>Neutral: { props.resenas.neutral }</div>
+      <div>Bad: { props.resenas.bad }</div>
+  </div>
+  )
+}
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
@@ -8,7 +24,12 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Titulos titulo="Give Feedback" />
+      <button onClick={() => setGood(good+1)}>Good</button>
+      <button onClick={() => setNeutral(neutral+1)}>Neutral</button>
+      <button onClick={() => setBad(bad+1)}>Bad</button>
+      <Titulos titulo="Statistics" />
+      <Estadisticas resenas={{good, neutral, bad}} />
     </div>
   )
 }
