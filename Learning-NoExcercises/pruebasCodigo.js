a81b50ff6c90f1331839289b06604a8448bcc2af
alejosatizabal <alejosatizabal@yahoo.es>
2024-01-29 15:44:25 -0500

    Añadiendo pruebasCodigo.js

---
+++ Learning-NoExcercises/pruebasCodigo.js
@@ -0,0 +1,19 @@
+  //let arreglo = ['uno','dos','tres']
+  //console.log(arreglo.filter( item => item = 'u' ));
+
+  //let palabra = 'palAbra'
+  //let palabra2 = palabra.toLocaleLowerCase()
+  //console.log(palabra2);
+  //console.log( palabra2.includes('lab') );
+
+  //console.log('persons: ', persons);
+  //console.log('newName: ', newName);
+
+  //console.log( persons.map(person => (person.name == 1 ? person : null ) ) );
+  //console.log(persons);
+  //console.log( persons.map(person => (person.name.includes('las') ? person : null ) ) );
+
+  //console.log( persons.filter( (persona) => persona.name != '' ) );
+  //console.log( persons.filter( (persona) => (persona.name).includes('rt') == true ) ); // newFilter
+  //console.log( persons.filter( (persona) => ( (persona.name).toLocaleLowerCase).includes('to') ) );
+
