9e51ba2cf89d19e3363f168df1b167d41ae032c1
alejosatizabal <alejosatizabal@yahoo.es>
2024-02-22 13:07:59 -0500

    Parte3.c Hasta 'Otras operaciones'

--- Learning-NoExcercises/apr-part3/index.js
+++ Learning-NoExcercises/apr-part3/index.js
@@ -11,23 +11,29 @@ app.use(express.json()) // Para hacer el POST
 
 app.use(express.static('dist')) // Para que el BackEnd redireccione a 'dist', el Front build
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/favicon.ico', (req, res) => {
   res.sendFile('favicon.ico');
@@ -42,17 +48,30 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
+  //.catch(error => {
+    //console.log(error)
+    //response.status(500).end()
+    //response.status(400).send({ error: 'malformatted id' })
+
+  //})
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -71,6 +90,27 @@ app.post('/api/notes', (request, response) => {
     response.json(savedNote)
   })
 })
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+// controlador de solicitudes con endpoint desconocido
+app.use(unknownEndpoint)
+
+// este debe ser el último middleware cargado
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
